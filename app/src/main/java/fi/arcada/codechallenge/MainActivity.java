60ca4758ba865a8721537ada5dd1ced555204937
Ben Lundström <ben.lundstrom@arcada.fi>
2024-03-25 15:40:40 +0200

    Code challenge 2

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,9 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    EditText inputText;
+
+    double[] values = {3.5, 4.4, 9.0, 1.2, 6.5, 12.0};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.outText);
+        inputText = findViewById(R.id.inputText);
+
+        outputText.setText("Min app funkar!");
+
+
     }
-}
+
+    public void buttonHandler(View view) {
+        outputText.setText(inputText.getText().toString());
+    }
+
+    public void calculate(View view) {
+
+            outputText.setText(String.format("%.2f", Statistics.calcMean(values)));
+        }
+
+    }
